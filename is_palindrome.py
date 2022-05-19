5885e662600194fd5f879670a04aec68a5d79a68
Botir-01 <xasanovich01@icloud.com>
2022-05-19 12:25:34 +0500

    added descriptions to problems

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,3 +1,9 @@
+# Given an integer x, return true if x is palindrome integer.
+#
+# An integer is a palindrome when it reads the same backward as forward.
+#
+# For example, 121 is a palindrome while 123 is not.
+# Follow up: Could you solve it without converting the integer to a string?
 def is_palindrome():
     input_number = int(input('Enter a number: '))
     initial_number = input_number
